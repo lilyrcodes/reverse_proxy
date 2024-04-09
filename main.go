b9171ec86e3321082cd42ccbe3fbf9eb274559f7
Lily Richards <lilymichellerichards@gmail.com>
2024-04-09 09:40:35 -0400

    Add https support

--- main.go
+++ main.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"context"
+	"crypto/tls"
 	"encoding/json"
 	"fmt"
 	"net/http"
 	"net/http/httputil"
 	"net/url"
 	"os"
+	"sync/atomic"
 	"time"
 )
 
@@ -20,13 +23,19 @@ func (m *UrlMapping) matches(u *url.URL) bool {
 	return m.Host == u.Host && m.Scheme == u.Scheme
 }
 
+type TlsFiles struct {
+	CertFile string `json:"cert_file"`
+	KeyFile  string `json:"key_file"`
+}
+
 type Config struct {
 	Mapping   []UrlMapping `json:"mapping"`
+	TlsFiles  []TlsFiles   `json:"tls_files"`
 	HttpPort  uint16       `json:"http_port"`
 	HttpsPort uint16       `json:"https_port"`
 }
 
-func ReadConfig(fname string) (Config, error) {
+func readConfig(fname string) (Config, error) {
 	var config Config
 	b, err := os.ReadFile(fname)
 	if err != nil {
@@ -39,18 +48,61 @@ func ReadConfig(fname string) (Config, error) {
 	return config, nil
 }
 
+func (c *Config) LoadTLSConfig() (*tls.Config, error) {
+	tlsConfig := &tls.Config{}
+	if len(c.TlsFiles) > 0 {
+		tlsConfig.Certificates = make([]tls.Certificate, len(c.TlsFiles))
+	}
+	var err error
+	for i, pair := range c.TlsFiles {
+		tlsConfig.Certificates[i], err = tls.LoadX509KeyPair(pair.CertFile, pair.KeyFile)
+		if err != nil {
+			return nil, err
+		}
+	}
+	return tlsConfig, nil
+}
+
+func makeServer(port uint16, proxy *httputil.ReverseProxy, tlsConfig *tls.Config) *http.Server {
+	return &http.Server{
+		Addr:           fmt.Sprintf("localhost:%d", port),
+		Handler:        &ProxyHandler{proxy},
+		TLSConfig:      tlsConfig,
+		ReadTimeout:    time.Second * 15,
+		WriteTimeout:   time.Second * 15,
+		MaxHeaderBytes: 2 >> 16,
+	}
+
+}
+
+func shutdownFunc(other *http.Server, isOtherDone *atomic.Bool, complete chan bool) func() {
+	return func() {
+		if !isOtherDone.Swap(true) {
+			if err := other.Shutdown(context.Background()); err != nil {
+				panic(err)
+			}
+		} else {
+			close(complete)
+		}
+	}
+}
+
 func main() {
 	if len(os.Args) != 2 {
 		fmt.Println("Usage: reverse_proxy <config_file>")
 		return
 	}
-	config_file := os.Args[1]
-	config, err := ReadConfig(config_file)
+	configFile := os.Args[1]
+	config, err := readConfig(configFile)
+	if err != nil {
+		panic(err)
+	}
+	tlsConfig, err := config.LoadTLSConfig()
 	if err != nil {
 		panic(err)
 	}
 
-	proxy := httputil.ReverseProxy{
+	proxy := &httputil.ReverseProxy{
 		Rewrite: func(r *httputil.ProxyRequest) {
 			for _, m := range config.Mapping {
 				if m.matches(r.In.URL) {
@@ -65,21 +117,17 @@ func main() {
 			return
 		},
 	}
-	http_server := http.Server{
-		Addr:    fmt.Sprintf("localhost:%d", config.HttpPort),
-		Handler: &ProxyHandler{&proxy},
+	httpServer := makeServer(config.HttpPort, proxy, nil)
+	httpsServer := makeServer(config.HttpsPort, proxy, tlsConfig)
 
-		TLSConfig:   nil,
-		ReadTimeout: time.Second * 15,
+	var isOtherDone *atomic.Bool
+	shutdownComplete := make(chan bool)
+	httpServer.RegisterOnShutdown(shutdownFunc(httpsServer, isOtherDone, shutdownComplete))
+	httpsServer.RegisterOnShutdown(shutdownFunc(httpServer, isOtherDone, shutdownComplete))
 
-		WriteTimeout: time.Second * 15,
-
-		MaxHeaderBytes: 2 >> 16,
-	}
-	err = http_server.ListenAndServe()
-	if err != nil {
-		panic(err)
-	}
+	go httpServer.ListenAndServe()
+	go httpsServer.ListenAndServeTLS("", "")
+	<-shutdownComplete
 }
 
 type ProxyHandler struct {
